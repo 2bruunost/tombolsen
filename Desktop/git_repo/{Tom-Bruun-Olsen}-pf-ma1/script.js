40b470443da408eb3e6a740a30ee84b9046966e9
2bruunost <tomrbuunolsen@gmail.com>
2020-09-27 16:38:56 +0200

    script.js

---
+++ Desktop/git_repo/{Tom-Bruun-Olsen}-pf-ma1/script.js
@@ -0,0 +1,30 @@
+// question 1
+var firstVar;
+
+// question 2
+var name = "Tom";
+
+// question 3
+var numberOne = 1;
+
+// question 4
+var division = 20 / 5;
+
+// question 5
+var frog = "Animal";
+typeof frog;
+
+// question 6
+var orderHasShipped = true;
+
+// question 7
+if (orderHasShipped === true) {
+	console.log("True");
+} else {
+	console.log("False");
+}
+
+// question 8
+for (i = 0; i <= 9; i++) {
+	console.log(i);
+}
